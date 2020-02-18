981d654e65c34e57ec5820b5e20bd81eec70984b
Quentin-Allen Velayo Fernandez <qfernandez@csumb.edu>
2020-02-17 16:21:23 -0800

    Initial Commit

---
+++ Baseball Stats Storing App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baseball Stats Storing App
+//
+//  Created by Quentin-Allen Velayo Fernandez on 2/17/20.
+//  Copyright © 2020 Quentin&Mary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
